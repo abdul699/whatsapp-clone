cb62a0e831486e945d134b3b284b1bff67cf568f
Abdul <kabdulsalam6@gmail.com>
2020-11-18 12:42:00 +0530

    added chat section and input fields

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,8 +1,8 @@
-import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import { Avatar, Icon, IconButton } from '@material-ui/core'
+import { AttachFile, InsertEmoticon, Message, MoreVert, SearchOutlined, SettingsInputAntenna } from '@material-ui/icons'
+import MicIcon from '@material-ui/icons/Mic'
 import React from 'react'
 import './Chat.css'
-// import { Avatar } from '@material-ui/core'
 
 function Chat() {
     return (
@@ -28,11 +28,44 @@ function Chat() {
                 </div>
             </div>
             <div className="chat__body">
-                
+                <p className="chat__message">
+                    <span className="chat__name">
+                        Abdul
+                    </span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__receiver chat__message">
+                    <span className="chat__name">
+                        Abdul
+                    </span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+            </div>
+            <div className="chat__footer">
+                <IconButton>
+                    <InsertEmoticon />
+                </IconButton>
+                <form>
+                    <input placeholder="Type a message"
+                        type="text"
+                    />
+                    <button type="submit"></button>
+                </form>
+                <IconButton>
+                    <MicIcon />
+                </IconButton>
+
             </div>
         </div>
     )
 }
 
 export default Chat
- 
