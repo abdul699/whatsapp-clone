4dcf67c0577d05802becd76c11fd932ce4a93c53
Abdul <kabdulsalam6@gmail.com>
2020-11-18 11:28:07 +0530

    Added chat header

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,12 +1,38 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 import React from 'react'
 import './Chat.css'
+// import { Avatar } from '@material-ui/core'
 
 function Chat() {
     return (
         <div class="chat">
-            <h1>Chat Component Here</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Chat Room</h3>
+                    <p>Last seen......</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+
+                </div>
+            </div>
+            <div className="chat__body">
+                
+            </div>
         </div>
     )
 }
 
 export default Chat
+ 
