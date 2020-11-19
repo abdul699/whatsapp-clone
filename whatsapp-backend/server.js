d372130abd84cb4d0f83a700cf3ead1f5bc735d3
Abdul <kabdulsalam6@gmail.com>
2020-11-19 16:23:07 +0530

    refactored the code

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,49 @@
+// importing
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+// app config
+
+const app = express()
+const port = process.env.PORT || 9000
+
+// middleware
+app.use(express.json())
+
+// DB config
+const connection_url = 'mongodb+srv://admin:co5Z9VAI1w7HFiKK@cluster0.obp4q.mongodb.net/whatsappdb?retryWrites=true&w=majority'
+
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("DataBase connected...")
+})
+
+// ???????
+
+// API routes
+app.get("/", (req, res) => {
+    res.status(200).send('hello world')
+})
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if(err) {
+            res.status(500).send(err)
+        }
+        else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+// Listen
+app.listen(port, () => console.log(`Listening on localhost: ${port}`));
